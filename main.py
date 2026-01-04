b7915aa09ce17d5396504bf09efed217f637f701
Immaculate <manjory.sjsu@gmail.com>
2026-01-03 23:28:17 -0800

    Initial commit: CSV filtering and timing experiment

---
+++ main.py
@@ -0,0 +1,63 @@
+import csv
+import os
+import time
+from datetime import datetime
+
+start = time.perf_counter()
+
+file_path='/Users/macbookpro_2015/dev/python/data-proc-timer/test_data_1000.csv'
+output_script_path = '/Users/macbookpro_2015/dev/python/data-proc-timer/filter_response.csv'
+
+file_size = os.path.getsize(file_path)
+print(f"File size: {file_size / 1024:.2f} KB")
+
+
+threshold_date=datetime(2022,1,1)
+
+with open(file_path, mode='r',newline='',encoding ='utf-8') as file:
+    reader=csv.DictReader(file)
+    header=reader.fieldnames
+
+    filtered_data=[]
+
+    for row in reader:
+        subs_date=row.get("Subscription Date","")
+
+        try:
+            sub_date_obj = datetime.strptime(subs_date, "%Y-%m-%d")
+            # print(sub_date_obj)
+
+            if sub_date_obj>=threshold_date:
+                filtered_data.append(row)
+        except ValueError:
+            continue
+
+with open(output_script_path,mode='w',newline='',encoding='utf-8') as output_file:
+    writer=csv.DictWriter(output_file,fieldnames=header)
+    writer.writeheader()
+    writer.writerows(filtered_data)
+
+
+end=time.perf_counter()
+
+elapsed=end-start
+
+print(f"Processed 1000 rows in {elapsed:.6f} seconds")
+
+rows=1000
+
+rows_per_sec=rows/elapsed
+
+print(f"Throughput:{rows_per_sec: .2f} rows/sec" )
+
+
+"""
+
+result from this processing:
+
+
+File size: 167.21 KB
+Processed 1000 rows in 0.016084 seconds
+Throughput: 62172.94 rows/sec
+
+"""
